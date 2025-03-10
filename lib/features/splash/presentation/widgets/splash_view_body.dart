44fa3d69111d85e2fb9b14f08b06859c4eeb34a3
Mohamed Seedyk <mohamedseedk908@gmail.com>
2025-03-10 12:07:44 +0200

    add animation in solash screen

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600),);
+    fadingAnimation =
+        Tween<double>(begin: .2, end: 1).animate(animationController!);
+    animationController?.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +27,17 @@ class SplashViewBody extends StatelessWidget {
       child: Column(
         children: [
           Spacer(),
-          Text(
-            "Fruits Market",
-            style: TextStyle(
-              fontFamily: "Poppins",
-              fontSize: 51,
-              color: Color(
-                0xffffffff,
-              ),
-              fontWeight: FontWeight.bold
+          FadeTransition(
+            opacity: fadingAnimation!,
+            child: Text(
+              "Fruits Market",
+              style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 51,
+                  color: Color(
+                    0xffffffff,
+                  ),
+                  fontWeight: FontWeight.bold),
             ),
           ),
           Spacer(),
